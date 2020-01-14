92ebdcbb059929225b5971f51ac2d900a2c549e6
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2020-01-14 21:34:47 +0100

    Add responsive layout

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
+/* curent date */
 document.getElementById('today').value = new Date().toISOString().slice(0, 10);
 document.getElementById('today2').value = new Date().toISOString().slice(0, 10);
+
+/* mobile menu */
+
+// function toggleMenu(visible) {
+//     document.querySelector('.sidebar').classList.toggle('show', visible);
+// }
+// console.log(visible);
